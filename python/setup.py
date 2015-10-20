3eed3fcef938eea0c29384c45db956fdec42fdb3
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2015-10-20 13:08:23 +0900

    クオートを変更

--- python/setup.py
+++ python/setup.py
@@ -2,17 +2,17 @@ import py2exe
 from distutils.core import setup
 
 option = {
-    "compressed": 1,
-    "optimize": 2,
-    "bundle_files": 1
+    'compressed': 1,
+    'optimize': 2,
+    'bundle_files': 1
 }
 
 setup(
     options={
-        "py2exe": option
+        'py2exe': option
     },
     console=[
-        {"script": "../word_counter.py"}
+        {'script': '../word_counter.py'}
     ],
     zipfile=None
 )
